197cc9520b956e84bce5f9139265ce735a37eec9
Bernard Tatin <bernard.tatin@outlook.fr>
2017-12-15 23:38:00 +0100

    First commit

---
+++ src/main/java/bernard/tatin/App.java
@@ -0,0 +1,13 @@
+package bernard.tatin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
