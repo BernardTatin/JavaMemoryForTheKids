40dec4f0e6a132997fc0bcd4462c82d064865355
Bernard Tatin <bernard.tatin@outlook.fr>
2017-12-26 08:15:32 +0100

    Refactoring of constants

---
+++ src/main/java/bernard/tatin/Constants/LinuxConstants.java
@@ -0,0 +1,7 @@
+package bernard.tatin.Constants;
+
+public class LinuxConstants {
+    public static final int PAGE_SIZE = 4096;
+    public static final int KILOBYTE = 1024;
+    public static final int MEGABYTE = LinuxConstants.KILOBYTE * LinuxConstants.KILOBYTE;
+}
