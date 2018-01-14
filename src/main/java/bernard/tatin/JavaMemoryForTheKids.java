08fd755f9b83e3c7e69f2e5bdc727568f4c4b7f6
Bernard Tatin <bernard.tatin@outlook.fr>
2018-01-14 14:59:50 +0100

    Better console output

--- src/main/java/bernard/tatin/JavaMemoryForTheKids.java
+++ src/main/java/bernard/tatin/JavaMemoryForTheKids.java
@@ -1,14 +1,11 @@
 package bernard.tatin;
 
-import bernard.tatin.Constants.ApplicationConstants;
-import bernard.tatin.Constants.LinuxConstants;
 import bernard.tatin.ProcFS.ProcessCommandLine;
 import bernard.tatin.ProcFS.ProcessID;
 import bernard.tatin.ProcFS.StatM;
 import bernard.tatin.Threads.ThPrinter;
 import bernard.tatin.Threads.ThPrinterClient;
 import bernard.tatin.Tools.Counter;
-import bernard.tatin.Tools.ForStrings;
 import bernard.tatin.Tools.ThMemoryFiller;
 
 import java.util.Arrays;
@@ -32,7 +29,11 @@ class JavaMemoryForTheKids extends ThPrinterClient {
     private void showTitle() {
         if (titleLine == null) {
             String[] aTitle = StatM.getMainInstance().getStatsTitle();
-            titleLine = Arrays.stream(aTitle).reduce("", String::concat);
+            if (aTitle != null) {
+                titleLine = Arrays.stream(aTitle).reduce("", String::concat);
+            } else {
+                titleLine ="titleLine is NULLLLLL";
+            }
         }
 
         printStrings(
@@ -44,18 +45,13 @@ class JavaMemoryForTheKids extends ThPrinterClient {
     private void innerLoop() {
         while (true) {
             long memory_size = ThMemoryFiller.getMainInstance().getMemorySize();
-            String[] aString = StatM.getMainInstance().getStats();
+            String[] aString = StatM.getMainInstance().getStats(memory_size);
 
             if (count.getValue() == 0) {
                 showTitle();
             }
 
             if (aString != null) {
-                aString[ApplicationConstants.FIELD_COUNT-1] =
-                        ForStrings.leftFormat(String.valueOf(memory_size /
-                                LinuxConstants.MEGABYTE),
-                                ApplicationConstants.FIELD_LENGTH);
-
                 printString(Arrays.stream(aString).reduce("", String::concat));
             } else {
                 printError("ERROR reading statm file");
--- src/main/java/bernard/tatin/ProcFS/StatM.java
+++ src/main/java/bernard/tatin/ProcFS/StatM.java
@@ -9,42 +9,58 @@ public class StatM {
     private final static StatM ourInstance = new StatM();
     private String[] statsTitles = null;
 
+    private final int FPROG_SIZE = 0;
+    private final int FRESIDENT = 1;
+    private final int FDATA = 2;
+    private final int FJTOTAL = 3;
+    private final int FJMAX = 4;
+    private final int FJFREE = 5;
+    private final int FALLOCATED = 6;
+
     public static StatM getMainInstance() {
         return ourInstance;
     }
 
     public String[] getStatsTitle() {
         if (statsTitles == null) {
-            int i = 0;
             int field_len = ApplicationConstants.FIELD_LENGTH - 2;
             statsTitles = new String[ApplicationConstants.FIELD_COUNT];
-            statsTitles[i++] = ForStrings.rightFormat("Prog. Size", field_len) + " | ";
-            statsTitles[i++] = ForStrings.rightFormat("Resident", field_len) + "| ";
-            statsTitles[i++] = ForStrings.rightFormat("Share", field_len) + "| ";
-            statsTitles[i++] = ForStrings.rightFormat("JVM total memory", field_len) + "| ";
-            statsTitles[i++] = ForStrings.rightFormat("JVM max memory", field_len) + "| ";
-            statsTitles[i++] = ForStrings.rightFormat("Data", field_len) + "| ";
-            statsTitles[i++] = ForStrings.rightFormat("JVM free mem", field_len) + "| ";
-            statsTitles[i] = ForStrings.rightFormat("Allocated memory", field_len) + " |";
+            statsTitles[FPROG_SIZE] =
+                    ForStrings.rightFormat("Prog. Size", field_len) + " | ";
+            statsTitles[FRESIDENT] =
+                    ForStrings.rightFormat("Resident", field_len) + "| ";
+            statsTitles[FDATA] =
+                    ForStrings.rightFormat("Data", field_len) + "| ";
+            statsTitles[FJTOTAL] =
+                    ForStrings.rightFormat("JVM total memory", field_len) + "| ";
+            statsTitles[FJMAX] =
+                    ForStrings.rightFormat("JVM max memory", field_len) + "| ";
+            statsTitles[FJFREE] =
+                    ForStrings.rightFormat("JVM free mem", field_len) + "| ";
+            statsTitles[FALLOCATED] =
+                    ForStrings.rightFormat("Allocated memory", field_len) + " |";
         }
         return statsTitles;
     }
 
-    public String[] getStats() {
-        return innerGetStats();
-    }
-
-    private String[] innerGetStats() {
+    public String[] getStats(long allocatedMemory) {
+        long[] lstats = new long[ApplicationConstants.FIELD_COUNT];
         String[] stats = new String[ApplicationConstants.FIELD_COUNT];
-        String[] strStats = ForFiles.loadLinesFromfiles(LinuxProc.procPathName("statm"), "[ \n]");
+        String[] strStats = ForFiles.loadLinesFromfiles(
+                LinuxProc.procPathName("statm"),
+                "[ \n]");
         if (strStats != null) {
-            // ugly, ugly, ugly !!!
-            strStats[6] = String.valueOf(Runtime.getRuntime().freeMemory() / LinuxConstants.PAGE_SIZE);
-            strStats[4] = String.valueOf(Runtime.getRuntime().maxMemory() / LinuxConstants.PAGE_SIZE);
-            strStats[3] = String.valueOf(Runtime.getRuntime().totalMemory() / LinuxConstants.PAGE_SIZE);
-            for (int i = 0; i < strStats.length && i < ApplicationConstants.FIELD_COUNT; i++) {
-                long v = (Long.parseLong(strStats[i]) * LinuxConstants.PAGE_SIZE) / LinuxConstants.MEGABYTE;
-                stats[i] = ForStrings.leftFormat(String.valueOf(v), ApplicationConstants.FIELD_LENGTH - 3) + "M |";
+            lstats[FALLOCATED] = allocatedMemory;
+            lstats[FJFREE] = Runtime.getRuntime().freeMemory();
+            lstats[FJMAX] = Runtime.getRuntime().maxMemory();
+            lstats[FJTOTAL] = Runtime.getRuntime().totalMemory();
+            lstats[FPROG_SIZE] = Long.parseLong(strStats[0]) * LinuxConstants.PAGE_SIZE;
+            lstats[FRESIDENT] = Long.parseLong(strStats[1]) * LinuxConstants.PAGE_SIZE;
+            lstats[FDATA] = Long.parseLong(strStats[5]) * LinuxConstants.PAGE_SIZE;
+            for (int i = 0; i < ApplicationConstants.FIELD_COUNT; i++) {
+                stats[i] = ForStrings.leftFormat(
+                        String.valueOf(lstats[i] / LinuxConstants.MEGABYTE),
+                        ApplicationConstants.FIELD_LENGTH - 3) + "M |";
             }
             return stats;
         } else {
