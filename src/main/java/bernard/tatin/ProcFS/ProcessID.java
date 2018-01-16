c7ae9eb25a2620928da96052d329e3a8f7e5cbfc
Bernard Tatin <bernard.tatin@outlook.fr>
2018-01-16 07:26:47 +0100

    ProcessID : only static elements

--- src/main/java/bernard/tatin/ProcFS/ProcessID.java
+++ src/main/java/bernard/tatin/ProcFS/ProcessID.java
@@ -2,20 +2,15 @@ package bernard.tatin.ProcFS;
 
 
 public class ProcessID {
-    final private static ProcessID ourInstance = new ProcessID();
-    private long pid = -1;
+    private static long pid = -1;
 
-    private long innerGetPID() {
+    public static long getPID() {
         if (pid == -1) {
             String processName =
                     java.lang.management.ManagementFactory.getRuntimeMXBean().getName();
             pid = Long.parseLong(processName.split("@")[0]);
         }
         return pid;
-    }
-
-    public static long getPID() {
-        return ProcessID.ourInstance.innerGetPID();
     }
 
 
