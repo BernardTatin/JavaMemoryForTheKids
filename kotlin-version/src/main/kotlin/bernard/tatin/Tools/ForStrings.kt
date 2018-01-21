e4be95015bab4120fc00b00cc6a117c998ea7309
Bernard Tatin <bernard.tatin@outlook.fr>
2018-01-21 05:30:44 +0100

    Works on ForStrings

--- kotlin-version/src/main/kotlin/bernard/tatin/Tools/ForStrings.kt
+++ kotlin-version/src/main/kotlin/bernard/tatin/Tools/ForStrings.kt
@@ -5,22 +5,23 @@ import bernard.tatin.Constants.ApplicationConstants
 import java.util.stream.Stream
 
 object ForStrings {
-    private var whites: String? = null
+    private var whites: String = initWhites()
 
-    fun rightFormat(str: String, expectedLength: Int): String {
-        var str = str
-        val strLength = str.length()
+    fun rightFormat(inStr: String, expectedLength: Int): String {
+        var str = inStr
+        val strLength = str.length
         if (strLength < expectedLength) {
             str += getWhites(
                     Math.min(expectedLength - strLength, ApplicationConstants.MAX_STRING_LENGTH))
+            return str
         } else if (strLength > expectedLength) {
-            str = str.substring(0, expectedLength)
+            return str.substring(0, expectedLength)
         }
-        return str
+        return inStr
     }
 
     fun leftFormat(str: String, expectedLength: Int): String {
-        val strLength = str.length()
+        val strLength = str.length
         return if (strLength < expectedLength) {
             getWhites(
                     Math.min(expectedLength - strLength,
@@ -32,10 +33,13 @@ object ForStrings {
         }
     }
 
+    private fun initWhites() : String {
+        whites = " "
+        while (whites.length < ApplicationConstants.MAX_STRING_LENGTH)
+            whites += " "
+        return whites
+    }
     private fun getWhites(length: Int): String {
-        if (whites == null) {
-            whites = Stream.iterate("", { s -> " " }).limit(ApplicationConstants.MAX_STRING_LENGTH).reduce("", ???({ String.concat() }))
-        }
         return whites!!.substring(0, length)
     }
 }
