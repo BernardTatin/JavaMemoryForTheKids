b39bb53f897775c56bf1ce25df6ec716ee7cb171
Bernard Tatin <bernard.tatin@outlook.fr>
2018-01-22 19:13:59 +0100

    A better CounterTests

--- kotlin-version/src/main/kotlin/bernard/tatin/WorkInProgress/counting/tests/CounterTests.kt
+++ kotlin-version/src/main/kotlin/bernard/tatin/WorkInProgress/counting/tests/CounterTests.kt
@@ -10,13 +10,19 @@ class CounterTests(private val max: Int, private val iloops: Int) : ITest {
     override fun innerTest(currentLoop : Int) : Boolean {
         val counter = Counter(max);
         var i = 0;
+        var v = 0
         while (i < max) {
-            var v = counter.value
+            v = counter.value
             if (i != v) {
-                java.lang.System.err.println("ERROR: $name failed on loop $currentLoop with i = $i, counter = $v and max = $max")
+                java.lang.System.err.println("ERROR: $name failed on loop $currentLoop expected = $i get counter = $v with max = $max")
                 return false
             }
             i++
+        }
+        v = counter.value
+        if (v != 0) {
+            java.lang.System.err.println("ERROR: $name failed on loop $currentLoop expected = 0 get counter = $v with max = $max")
+            return false
         }
         return true
     }
