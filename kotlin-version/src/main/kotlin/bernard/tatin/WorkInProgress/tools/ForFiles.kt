27a060f6af991fa1f4cfdb9f5cb58dcd60a77fc3
Bernard Tatin <bernard.tatin@outlook.fr>
2018-01-23 01:08:39 +0100

    Starting ForFiles testing - does not compile

--- kotlin-version/src/main/kotlin/bernard/tatin/WorkInProgress/tools/ForFiles.kt
+++ kotlin-version/src/main/kotlin/bernard/tatin/WorkInProgress/tools/ForFiles.kt
@@ -6,17 +6,23 @@ import java.nio.file.Path
 object ForFiles {
     fun loadTextFile(path: Path): String? {
         try {
-            val aLine = Files.readAllBytes(path)
-            val l = aLine.size
+            val file = File(path)
+            val fileBytes : ByteArray = File.readBytes()
+            val l = fileBytes.size
 
             if (l > 0) {
                 // '\0' is a word separator
-                for (i in 0 until l) {
-                    if (aLine[i].toInt() == 0) {
-                        aLine[i] = 32
-                    }
-                }
-                return String(aLine, "UTF-8")
+                val chars : java.util.ArrayList<Byte> =
+                        java.util.ArrayList(
+                                fileBytes.map{
+                                    c -> return if (c.toInt == 0)  32 else c})
+                return chars.toString()
+//                for (i in 0 until l) {
+//                    if (fileBytes[i].toInt() == 0) {
+//                        fileBytes[i] = 32
+//                    }
+//                }
+//                return String(fileBytes, "UTF-8")
             } else {
                 return null
             }
@@ -26,7 +32,7 @@ object ForFiles {
 
     }
 
-    fun loadLinesFromFiles(path: Path, separators: String): Array<String>? {
+    fun loadLinesFromFiles(path: Path, separators: String): List<String>? {
         val fileContent = ForFiles.loadTextFile(path)
         return fileContent?.split(separators)
     }
