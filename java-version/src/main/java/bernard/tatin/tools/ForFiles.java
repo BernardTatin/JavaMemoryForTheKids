5fbe4cfdd3d17dfbe7e665b410e2ba5551a7930c
Bernard Tatin <bernard.tatin@outlook.fr>
2018-02-01 08:15:24 +0100

    Tried javax.inject.Singleton, failed...

--- java-version/src/main/java/bernard/tatin/tools/ForFiles.java
+++ java-version/src/main/java/bernard/tatin/tools/ForFiles.java
@@ -6,20 +6,20 @@ import java.nio.file.Path;
 public class ForFiles {
     public static String loadTextFile(Path path) {
         try {
-        byte[] aLine = Files.readAllBytes(path);
-        int l = aLine.length;
+            byte[] aLine = Files.readAllBytes(path);
+            int l = aLine.length;
 
-        if (l > 0) {
-            // '\0' is a word separator
-            for (int i=0; i<l; i++) {
-                if (aLine[i] == 0) {
-                    aLine[i] = 32;
+            if (l > 0) {
+                // '\0' is a word separator
+                for (int i=0; i<l; i++) {
+                    if (aLine[i] == 0) {
+                        aLine[i] = 32;
+                    }
                 }
+                return new String(aLine, "UTF-8");
+            } else {
+                return null;
             }
-            return new String(aLine, "UTF-8");
-        } else {
-            return null;
-        }
         }
         catch (Exception e) {
             return null;
