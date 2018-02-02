4fe8d6ac5c201fd01dfe38edff4df1c9300f7ccf
Bernard Tatin <bernard.tatin@outlook.fr>
2018-02-02 16:29:29 +0100

    Refactoring, for a more functionnal style

--- java-version/src/main/java/bernard/tatin/tools/ForFiles.java
+++ java-version/src/main/java/bernard/tatin/tools/ForFiles.java
@@ -1,25 +1,22 @@
 package bernard.tatin.tools;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import java.util.Arrays;
+import org.apache.commons.lang3.ArrayUtils;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 
 public class ForFiles {
     public static String loadTextFile(Path path) {
         try {
-            byte[] aLine = Files.readAllBytes(path);
-            int l = aLine.length;
-
-            if (l > 0) {
-                // '\0' is a word separator
-                for (int i=0; i<l; i++) {
-                    if (aLine[i] == 0) {
-                        aLine[i] = 32;
-                    }
-                }
-                return new String(aLine, "UTF-8");
-            } else {
-                return null;
-            }
+            Byte tableau[] = ArrayUtils.toObject(Files.readAllBytes(path));
+            tableau = Arrays.stream(tableau).map(b -> b == 0 ? 32 : b).toArray(Byte[]::new);
+            return new String(ArrayUtils.toPrimitive(tableau));
         }
         catch (Exception e) {
             return null;
