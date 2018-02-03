3ef1338ad8bec64a08932694f05f77b87e1d005d
Bernard Tatin <bernard.tatin@outlook.fr>
2018-02-03 19:42:19 +0100

    Using io.vavr.Option

--- java-with-libs/src/main/java/bernard/tatin/tools/ForFiles.java
+++ java-with-libs/src/main/java/bernard/tatin/tools/ForFiles.java
@@ -2,6 +2,7 @@ package bernard.tatin.tools;
 
 import static io.vavr.API.*;
 import static io.vavr.Predicates.*;
+import io.vavr.control.Option;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,7 +15,6 @@ public class ForFiles {
     public static String loadTextFile(Path path) {
         try {
             Byte tableau[] = ArrayUtils.toObject(Files.readAllBytes(path));
-			// io.vavr.Array<Byte> tableau = io.vavr.Array<Byte>.(Files.readAllBytes(path));
             tableau = Arrays.stream(tableau)
 					.map(b -> Match(b.intValue()).of(
 							Case($(is(0)), 32),
@@ -29,13 +29,11 @@ public class ForFiles {
     }
 
     public static String[] loadLinesFromFiles(Path path, String separators) {
-        String fileContent = ForFiles.loadTextFile(path);
-        if (fileContent != null) {
-            return fileContent.split(separators);
-        } else {
-            return null;
-        }
+        Option<String> oFileContent = Option.of(ForFiles.loadTextFile(path));
+
+        return oFileContent.getOrElse("").split(separators);
     }
+
     private ForFiles() {
     }
 }
